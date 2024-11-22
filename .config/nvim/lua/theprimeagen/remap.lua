8f5b3d3f5fa545bb83a3cf9540105a790bba5bb0
Jack Sovern <25jaso@decorah.school>
2024-11-22 13:11:38 -0600

    idk

--- .config/nvim/lua/theprimeagen/remap.lua
+++ .config/nvim/lua/theprimeagen/remap.lua
@@ -37,9 +37,7 @@ vim.keymap.set("x", "<leader>p", [["_dP]]);
 
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set("n", "<leader>y", [["+Y]])
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
